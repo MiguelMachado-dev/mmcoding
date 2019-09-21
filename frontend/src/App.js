1ffb5ef5caaa6bb7353be7f03051d261a2b57883
Miguel Machado <miiguellmp@gmail.com>
2019-09-21 13:20:50 -0300

    Analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import Contact from "./components/Contact";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +13,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
